20e456091ba40700c0e65f0def4a6e3097c1d90b
Abdullahi Oderinde <abdullahiolalekan2003.com>
2025-06-15 03:32:40 +0100

    Update app.py and add Procfile for deployment

--- app.py
+++ app.py
@@ -237,5 +237,7 @@ def internal_error(error):
 with app.app_context():
     db.create_all()
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
